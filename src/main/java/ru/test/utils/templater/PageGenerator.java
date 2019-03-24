fc3e71dd9773ed9047020d1f45d6e07ce93d7a2a
DevAzz <lyashkosergey2015@gmail.com>
2019-03-24 21:00:16 +0300

    Доработка отображения сообщений на странице

--- src/main/java/ru/test/utils/templater/PageGenerator.java
+++ src/main/java/ru/test/utils/templater/PageGenerator.java
@@ -1,5 +1,6 @@
 package ru.test.utils.templater;
 
+import freemarker.core.Environment;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -8,6 +9,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.util.Locale;
 import java.util.Map;
 
 public class PageGenerator {
@@ -25,8 +27,11 @@ public class PageGenerator {
     public String getPage(String filename, Map<String, Object> data) {
         Writer stream = new StringWriter();
         try {
-            Template template = cfg.getTemplate(HTML_DIR + File.separator + filename);
-            template.process(data, stream);
+            Template template = cfg.getTemplate(HTML_DIR + File.separator + filename,
+                                                Locale.getDefault(), "UTF-8");
+            Environment env = template.createProcessingEnvironment(data, stream);
+            env.setOutputEncoding("UTF-8");
+            env.process();
         } catch (IOException | TemplateException e) {
             e.printStackTrace();
         }
@@ -35,5 +40,6 @@ public class PageGenerator {
 
     private PageGenerator() {
         cfg = new Configuration();
+        cfg.setEncoding(Locale.getDefault(), "UTF-8");
     }
 }
